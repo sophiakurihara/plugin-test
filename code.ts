d0e6225af7706bd6bff610bd675b45348a6fa85d
Sophia Kurihara <sophiakurihara@Sophia-Kuriharas-computer.local>
2021-06-29 14:10:45 -0500

    tweet creation ui

--- code.ts
+++ code.ts
@@ -1,3 +1,4 @@
+
 // This plugin will open a window to prompt the user to enter a number, and
 // it will then create that many rectangles on the screen.
 
@@ -14,17 +15,31 @@ figma.showUI(__html__);
 figma.ui.onmessage = msg => {
   // One way of distinguishing between different types of messages sent from
   // your HTML page is to use an object with a "type" property like this.
-  if (msg.type === 'create-rectangles') {
+  //if (msg.type === 'create-rectangles') {
+  //  const nodes: SceneNode[] = [];
+  //  for (let i = 0; i < msg.count; i++) {
+  //    const rect = figma.createRectangle();
+  //    rect.x = i * 150;
+  //    rect.fills = [{type: 'SOLID', color: {r: 0, g: 0, b: 0}}];
+  //    figma.currentPage.appendChild(rect);
+  //    nodes.push(rect);
+  //  }
+  //  figma.currentPage.selection = nodes;
+  //  figma.viewport.scrollAndZoomIntoView(nodes);
+  //}
+
+  if(msg.type === 'create-tweet') {
     const nodes: SceneNode[] = [];
-    for (let i = 0; i < msg.count; i++) {
-      const rect = figma.createRectangle();
-      rect.x = i * 150;
+
+    const rect = figma.createRectangle();
+      rect.x = 250;
       rect.fills = [{type: 'SOLID', color: {r: 0, g: 0, b: 0}}];
       figma.currentPage.appendChild(rect);
+      
       nodes.push(rect);
-    }
-    figma.currentPage.selection = nodes;
-    figma.viewport.scrollAndZoomIntoView(nodes);
+
+      figma.currentPage.selection = nodes;
+      figma.viewport.scrollAndZoomIntoView(nodes);
   }
 
   // Make sure to close the plugin when you're done. Otherwise the plugin will
