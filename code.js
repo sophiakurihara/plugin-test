5639d896de17aad521bdf9f0621086bd60179a0d
Sophia Kurihara <sophiakurihara@Sophia-Kuriharas-computer.local>
2021-06-29 16:39:38 -0500

    removed rectangle creator

--- code.js
+++ code.js
@@ -11,19 +11,12 @@ figma.showUI(__html__);
 figma.ui.onmessage = msg => {
     // One way of distinguishing between different types of messages sent from
     // your HTML page is to use an object with a "type" property like this.
-    //if (msg.type === 'create-rectangles') {
-    //  const nodes: SceneNode[] = [];
-    //  for (let i = 0; i < msg.count; i++) {
-    //    const rect = figma.createRectangle();
-    //    rect.x = i * 150;
-    //    rect.fills = [{type: 'SOLID', color: {r: 0, g: 0, b: 0}}];
-    //    figma.currentPage.appendChild(rect);
-    //    nodes.push(rect);
-    //  }
-    //  figma.currentPage.selection = nodes;
-    //  figma.viewport.scrollAndZoomIntoView(nodes);
-    //}
     if (msg.type === 'create-tweet') {
+        const frameWidth = 800;
+        const frameHeight = 600;
+        const frame = figma.createFrame();
+        frame.resizeWithoutConstraints(frameWidth, frameHeight);
+        frame.fills = [{ type: 'SOLID', color: { r: 0, g: 0, b: 0 } }];
         const nodes = [];
         const rect = figma.createRectangle();
         rect.x = 250;
